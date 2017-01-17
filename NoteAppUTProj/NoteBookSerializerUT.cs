da44294142ee4dd8ec58ad9943cf88d2f6ef796a
John Samuel <johnsavkel@gmail.com>
2017-01-17 23:22:48 +0530

    Add files via upload

---
+++ NoteAppUTProj/NoteBookSerializerUT.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoteBookObj;
+
+namespace NoteAppUTProj
+{
+    [TestClass]
+    public class NoteBookSerializerUT
+    {
+        //dependecy injection
+        private NoteList allNotes = new NoteList();
+        private NoteList displayNotes = new NoteList();
+
+        private NoteListSerializer serializer = new NoteListSerializer();
+        private const String NOTES_FILENAME = "notes.bin";
+        private const String NOTES_BACKUP_FILENAME = "notes.bak";
+
+
+        [TestMethod]
+        public void TestCreateNote()
+        {
+            try
+            {
+               allNotes.Add(new Note());
+            }
+            catch(Exception ex)
+            {
+                StringAssert.Contains(ex.Message, "out of memory");
+            }
+
+        }
+        [TestMethod]
+        public void TestCreateNotesInLoop()
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    allNotes.Add(new Note());
+                }
+            }
+            catch (Exception ex)
+            {
+                StringAssert.Contains(ex.Message, "out of memory");
+            }
+
+        }
+        [TestMethod]
+        public void TestEditNote()
+        {
+        }
+        [TestMethod]
+        public void TestDeleteNote()
+        {
+        }
+        [TestMethod]
+        public void TestSyncNotes()
+        {
+        }
+    }
+}
---
+++ NoteAppUTProj/NoteBookSyncUT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoteBookSync;
+using NoteApp;
+
+namespace NoteAppUTProj
+{
+    [TestClass]
+    public class NoteBookSyncUT
+    {
+        [TestMethod]
+        //Test the data sync basics
+        public void TestSyncFileSystemReplicasOneWay()
+        {
+            //@read configuration
+            NoteBookSync.NoteBookFileSyncProvider objTest = new NoteBookFileSyncProvider();
+            NoteAppPreference pref = new NoteAppPreference();
+            bool bSync = objTest.SyncFileSystemReplicas(pref.GetLocalServer(), pref.GetServerInfo());
+            Assert.AreEqual(bSync, true, "Sync not happened properly");
+        }
+        [TestMethod]
+        public void TestDetectChangesOnFileSystemReplica()
+        {
+            NoteBookSync.NoteBookFileSyncProvider objTest = new NoteBookFileSyncProvider();
+            NoteAppPreference pref = new NoteAppPreference();
+            bool bSync = objTest.DetectChangesOnFileSystemReplica(pref.GetServerInfo());
+        }
+    }
+}
